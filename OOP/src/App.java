655a2fd9ca18b658f517f9cbced4ecb6e020a227
Adtchamp <adithepbunim@gmail.com>
2025-10-04 15:36:47 +0700

    test

--- OOP/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Java kak mak");
-        System.out.println("หวัดดีวัยรุ่น");
-    }
-}
