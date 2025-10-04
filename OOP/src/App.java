434de392a510f3b283cd56c0e66e74503d8d759f
Adtchamp <87360433+Tempuradesu@users.noreply.github.com>
2025-10-04 15:09:47 +0700

    Update App.java

--- OOP/src/App.java
+++ OOP/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Java kak mak");
+        System.out.println("หวัดดีวัยรุ่น");
     }
 }
